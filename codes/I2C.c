f6c9c147732689e41c240f596af52c436704aaaf
Shaurya-Aggarwal07 <shaurya_2301ee56@iitp.ac.in>
2025-11-13 12:04:37 +0530

    added code

--- codes/I2C.c
+++ codes/I2C.c
@@ -2,39 +2,43 @@
 #include <Wire.h>
 
 void setup() {
-  Wire.begin(); // Join I2C bus as master
   Serial.begin(9600);
-  delay(1000); // Give some time for setup
+
+  Wire.begin(); // Join I2C bus as master  
+  delay(1000); 
 }
 
 void loop() {
-  Wire.beginTransmission(8); // Address of slave (must match slave's Wire.begin(8))
+  Wire.beginTransmission(8); // Address of slave
   Wire.write("Hello from Master");
   Wire.endTransmission();
 
   Serial.println("Sent to Slave: Hello from Master");
 
-  delay(1000); // Send every 1 second
+  delay(1000); 
 }
 
 //---------------------------------------------------------
 // SLAVE
 #include <Wire.h>
 
-void setup() {
-  Wire.begin(8);        // Set I2C address to 8
-  Wire.onReceive(receiveEvent);
-  Serial.begin(9600);
+void setup(){
+    Serial.begin(9600);
+
+    Wire.begin(8);        // Set I2C slave address to 8
+    Wire.onReceive(fn);
 }
 
 void loop() {
-  // Do nothing in loop
+ 
 }
 
-void receiveEvent(int bytes) {
+void fn(int bytes) {
+    
   while (Wire.available()) {
     char c = Wire.read();
     Serial.print(c);
   }
+
   Serial.println();
 }
